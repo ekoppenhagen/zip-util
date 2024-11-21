b64f1b4e6009bd0b4dcfdc99a0303b84e91bec44
Erik Koppenhagen <erik.koppenhagen@iits-consulting.de>
2024-11-21 09:06:48 +0100

    chore: update project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-bom
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.0.21"
     // https://mvnrepository.com/artifact/io.gitlab.arturbosch.detekt/detekt-gradle-plugin
-    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("io.gitlab.arturbosch.detekt") version "1.23.7"
 }
 
 group = "com.github.ekoppenhagen"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories(RepositoryHandler::mavenCentral)
 kotlin { jvmToolchain(21) }
 detekt {
     // https://mvnrepository.com/artifact/io.gitlab.arturbosch.detekt/detekt-gradle-plugin
-    toolVersion = "1.23.6"
+    toolVersion = "1.23.7"
     config.from(file("config/detekt.yml"))
     buildUponDefaultConfig = true
     allRules = false
@@ -24,13 +24,13 @@ testing {
     suites {
         val test by getting(JvmTestSuite::class) {
             // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
-            useJUnitJupiter("5.10.2")
+            useJUnitJupiter("5.11.3")
         }
     }
 }
 dependencies {
     // https://mvnrepository.com/artifact/io.gitlab.arturbosch.detekt/detekt-gradle-plugin
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
     // https://mvnrepository.com/artifact/io.kotest/kotest-runner-junit5
-    testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
 }
